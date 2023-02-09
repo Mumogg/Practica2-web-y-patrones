cf727f6cdcab0749f493cfa07b45d9830a8bfd8c
Sebastian Murillo <104529023+Mumogg@users.noreply.github.com>
2023-02-09 14:43:21 -0600

    v1

---
+++ src/test/java/com/Practicas2/Practica/PracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practicas2.Practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
